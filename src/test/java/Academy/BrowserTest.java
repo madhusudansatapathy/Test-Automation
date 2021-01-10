4a4d5c2eeca2bb18943c5b14d5ced7f8e3f08f16
madhusudansatapathy <59648214+madhusudansatapathy@users.noreply.github.com>
2021-01-11 00:33:19 +0530

    Update BrowserTest.java

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -22,7 +22,7 @@ public class BrowserTest {
 		driver.get("https://madhusudan2020.azurewebsites.net/webapp/");
 		String text =driver.findElement(By.cssSelector("h1")).getText();
 		System.out.println(text);
-		Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
+		Assert.assertTrue(text.equalsIgnoreCase("Test Automation"));
 		driver.close();
 	
 		
